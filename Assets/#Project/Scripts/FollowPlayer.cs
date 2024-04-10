fa78f6f4a8cd135dc308c6bb1e3b8a3e3ebabf83
Bastien <Bastien@ICT-103-00.brufor.lan>
2024-04-10 15:28:44 +0200

    partie 1 - extra

--- Assets/#Project/Scripts/FollowPlayer.cs
+++ Assets/#Project/Scripts/FollowPlayer.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class FollowPlayer : MonoBehaviour
 {
     [SerializeField] private float offset = -10;
     [SerializeField] Transform target;
+    [SerializeField] private float speed;
+
     void Update()
     {
+        float z = transform.position.z + speed * Time.deltaTime; 
         Vector3 position = transform.position;
-        position.z = target.position.z + offset;
+        position.z = Mathf.Min(target.position.z + offset, z);
         transform.position = position;
     }
 }
